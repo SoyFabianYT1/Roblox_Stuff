ad82d941817ad8898d4f15acd15dd18e6149d928
Fabian <70019387+SoyFabianYT1@users.noreply.github.com>
2024-05-19 20:22:19 -0500

    Update Functions.lua

--- Code/GetData.lua
+++ Code/GetData.lua
@@ -23,4 +23,26 @@ function Functions.GetData(Part, Name, Position, Size, Color, Material, Anchored
 	print(Data)
 end
 
+function Functions.AddBlock(Part)
+	local CD = Instance.new("ClickDetector")
+	local Debounce = false
+	
+	CD.Parent = Part
+	CD.MouseClick:Connect(function(player)
+		if player.Name == "SoyFabian_YT3" then
+			if Debounce == false then
+				Part.Transparency = 0
+				Part.Color = Color3.new(0,0,0)
+				Debounce = true
+			elseif Debounce == true then
+				Part.Transparency = 0.6
+				Part.Color = Color3.new(1, 1, 1)
+				Debounce = false
+			end
+		else
+			warn(player.Name.." Tried to open the window")
+		end
+	end)
+end
+
 return Functions
